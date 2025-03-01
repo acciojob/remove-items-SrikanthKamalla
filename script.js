377ee6aa9c9f70d861a8e9a40f43023f554f6640
Srikanth Kamalla <113651024+SrikanthKamalla@users.noreply.github.com>
2025-03-01 10:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorSelect=document.getElementById("colorSelect");
+let btn=document.querySelector("input");
+
+btn.addEventListener("click",function () {
+	let  input=colorSelect.selectedIndex;
+	colorSelect.remove(input);	
+})
